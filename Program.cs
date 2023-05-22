161d7598c2c3219b3e4b3a224fa735dd502017e2
rover131 <76446159+rover131@users.noreply.github.com>
2023-05-22 16:56:50 +0300

    kr1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace gbkontrolnaya
+{
+    class Program
+    {
+        static string[] ReadArray(){
+            return Console.ReadLine().Split();
+        }
+
+        static string[] LengthLessThanOrEqualTo3(string[] array){
+            string[] result = new string[array.Length];
+            int j = 0;
+            for(int i = 0; i < array.Length; i++){
+                if (array[i].Length <= 3)
+                {
+                    result[j] = array[i];
+                    j++;
+                }
+            }
+            return result;
+        }
+
+        static void WriteArray(string[] array){
+            for(int i = 0; i < array.Length; i++){
+                Console.Write(array[i] + ", ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an array: ");
+            WriteArray(LengthLessThanOrEqualTo3(ReadArray()));
+        }
+    }
+}
